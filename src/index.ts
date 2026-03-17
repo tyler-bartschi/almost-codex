f6bfaf4703d5fa28da3c6cfdf59d3339e05c2e8e
tyler-bartschi <tylerabartschi@gmail.com>
2026-03-17 11:45:50 -0600

    testing personality files

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,51 @@
-const testDebug = () => {
-  const num1 = 5;
-  const num2 = 5;
-  console.log(num1 + num2);
+import OpenAI from "openai";
+import * as fs from "fs";
+import * as yaml from "yaml";
+import promptSync from "prompt-sync";
+
+if (!process.env.OPENAI_API_KEY) {
+  throw new Error("OPENAI_API_KEY environment variable is not set.");
 }
 
-testDebug();
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const prompt = promptSync();
+
+async function main() {
+  try {
+    const rawContent = fs.readFileSync(
+      "src/personalities/efficient.yaml",
+      "utf-8",
+    );
+
+    // replace with the actual configuration type
+    const config = yaml.parse(rawContent) as any;
+
+    const systemPrompt = config.prompt;
+
+    const history: any[] = [{ role: "system", content: systemPrompt }];
+
+    let message: string = prompt("Enter a message: ");
+
+    while (true) {
+      history.push({ role: "user", content: message });
+
+      const response = await client.responses.create({
+        model: "gpt-5-nano",
+        input: history,
+      });
+      console.log(response.output_text);
+      
+      message = prompt("");
+      if (message === "") {
+        return;
+      }
+    }
+  } catch (error) {
+    console.error(`Error calling OpenAI API:`, error);
+  }
+}
+
+main();
